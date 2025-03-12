cc889643da3f9855c60901add5f6b887e7e83957
tranzbook <tranzbook1@gmail.com>
2025-03-12 00:22:32 +0000

    export

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -2,7 +2,7 @@ import NextAuth, { User } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { NextAuthOptions } from "next-auth";
 
-export const authOptions: NextAuthOptions = {
+ const authOptions: NextAuthOptions = {
   secret: process.env.NEXTAUTH_SECRET,
   debug: true,
   session: {
